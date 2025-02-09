4cc1fdc19e349229f82b2251efa3d7aebbdea8ec
Yanzhi <Yanzhiii@users.noreply.github.com>
2025-02-09 03:49:20 +0000

    update

--- utils.py
+++ utils.py
@@ -1,3 +1,9 @@
+import torch
+import numpy as np
+import pandas as pd
+from tqdm.autonotebook import tqdm
+import time
+
 def moveTo(obj, device):
     """
     obj: the python object to move to a device, or to move its contents to a device
@@ -18,3 +24,108 @@ def moveTo(obj, device):
         return obj.to(device)
     else:
         return obj
+    
+
+def run_epoch(model, optimizer, data_loader, loss_func, device,
+              results, score_funcs, prefix=" ", desc=None):
+    running_loss = []
+    y_true = []
+    y_pred = []
+    start = time.time()
+    for inputs, labels in tqdm(data_loader, desc=desc, leave=False):
+        #Move the batch to the device we are using. 
+        inputs = moveTo(inputs, device)
+        labels = moveTo(labels, device)
+
+        y_hat = model(inputs) #this just computed f_Θ(x(i))
+        # Compute loss.
+        loss = loss_func(y_hat, labels)
+
+        if model.training:
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+
+        #Now we are just grabbing some information we would like to have
+        running_loss.append(loss.item())
+
+        if len(score_funcs) > 0 and isinstance(labels, torch.Tensor):
+            #moving labels & predictions back to CPU for computing / storing predictions
+            labels = labels.detach().cpu().numpy()
+            y_hat = y_hat.detach().cpu().numpy()
+            #add to predictions so far
+            y_true.extend(labels.tolist())
+            y_pred.extend(y_hat.tolist())
+    #end training epoch
+    end = time.time()
+    
+    y_pred = np.asarray(y_pred)
+    if len(y_pred.shape) == 2 and y_pred.shape[1] > 1: #We have a classification problem, convert to labels
+        y_pred = np.argmax(y_pred, axis=1)
+    #Else, we assume we are working on a regression problem
+    
+    results[prefix + " loss"].append( np.mean(running_loss) )
+    for name, score_func in score_funcs.items():
+        try:
+            results[prefix + " " + name].append( score_func(y_true, y_pred) )
+        except:
+            results[prefix + " " + name].append(float("NaN"))
+    return end - start #time spent on epoch
+    
+
+
+def train_simple_network(model, loss_func, train_loader, test_loader=None,
+                         score_funcs=None, epochs=50, device="cpu", checkpoint_file=None):
+    '''
+    Train a simple neural network.
+
+    Args:
+        model: The PyTorch Module to run for one epoch, which represents the model.
+        loss_func: The loss function which takes two arguments, the model output y_hat and the labels y, and returns a loss to use for training.
+        train_loader: The DataLoader object that returns tuples of (input, label) pairs used for training the model.
+        test_loader: The DataLoader object that returns tuples of (input, label) pairs used for evaluating the model.
+        epochs: The number of training epochs to perform.
+        score_funcs: A dictionary of scoring functions to use to evaluate the performance of the model.
+        device: The compute location to perform training.
+        checkpoint_file: A string indicating the location to save model checkpoints to disk.
+    '''
+    to_track = ["epoch", "total time", "train loss"]
+    if test_loader is not None:
+        to_track.append("test loss")
+    for eval_score in score_funcs:
+        to_track.append("train " + eval_score )
+        if test_loader is not None:
+            to_track.append("test " + eval_score )
+        
+    total_train_time = 0 #How long have we spent in the training loop? 
+    results = {}
+    #Initialize every item with an empty list
+    for item in to_track:
+        results[item] = []
+        
+    #SGD is Stochastic Gradient Decent.
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.001)
+    #Place the model on the correct compute resource (CPU or GPU)
+    model.to(device)
+    for epoch in tqdm(range(epochs), desc="Epoch"):
+        model = model.train()#Put our model in training mode
+        
+        total_train_time += run_epoch(model, optimizer, train_loader, loss_func, device, results, score_funcs, prefix="train", desc="Training")
+
+        results["total time"].append( total_train_time )
+        results["epoch"].append( epoch )
+        
+        if test_loader is not None:
+            model = model.eval()
+            with torch.no_grad():
+                run_epoch(model, optimizer, test_loader, loss_func, device, results, score_funcs, prefix="test", desc="Testing")
+                    
+    if checkpoint_file is not None:
+        torch.save({
+            'epoch': epoch,
+            'model_state_dict': model.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+            'results' : results
+            }, checkpoint_file)
+
+    return pd.DataFrame.from_dict(results)
